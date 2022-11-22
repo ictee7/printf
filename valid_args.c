637ccfacb799f3a027f837d6704029b080b30654
Ian Chirwa <chirwaian7@gmail.com>
2022-11-22 10:45:32 +0200

    added valid_args.c file

---
+++ valid_args.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+  *valid_args - Check for valid args
+  *@fmt: first parameter passed
+  *@ap: second parameter passed
+  *Return: printed
+  */
+int valid_args(const char **fmt, va_list ap)
+{
+	int i, printed;
+
+	op_t ops[] = {
+		{"i", p_int},
+		{"d", p_d_int},
+		{NULL, NULL}
+	};
+
+	printed = 0;
+
+	for (i = 0; *(ops[i].f) != NULL; i++)
+	{
+		if (*fmt[0] == '%')
+		{
+			_putchar('%');
+			printed++;
+			break;
+		}
+
+		else if (*fmt[0] == *(ops[i].op))
+		{
+			printed += ops[i].f(ap);
+			break;
+		}
+	}
+
+	if (*(ops[i].f) == NULL)
+	{
+		_putchar('%');
+		_putchar(*fmt[0]);
+		printed += 2;
+	}
+	return (printed);
+}
+
