fe84ef592db4d900a7d2cf6d991465d80010d510
emmanuel mziya <mziyaemmanuel@gmail.com>
2022-11-22 04:17:22 -0800

    valid arguments

--- valid_args.c
+++ valid_args.c
@@ -11,17 +11,8 @@ int valid_args(const char **fmt, va_list ap)
 	int i, printed;
 
 	op_t ops[] = {
-		{"i", p_int},
-		{"d", p_d_int},
 		{"c", p_char},
 		{"s", p_string},
-		{"i", p_int},
-		{"d", p_d_int},
-		{"b", p_bin},
-		{"u", p_uint},
-		{"o", p_oct},
-		{"x", p_x_min},
-		{"X", p_x_may},
 		{NULL, NULL}
 	};
 
