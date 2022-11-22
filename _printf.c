3d2f4259174d0163178b38a997d83f1b7d26d092
Ian Chirwa <chirwaian7@gmail.com>
2022-11-22 06:22:44 -0800

    modified several files

--- _printf.c
+++ _printf.c
@@ -1,27 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-/**
- * _printf -function main entry.
- * Description: function that emulates printf.
- * @format: list of types of arguments passed to the function
- * Return: void
- **/
-int _printf(const char *format, ...)
-{
-	int i = 0;
-	va_list valist;
-
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-	va_start(valist, format);
-
-	for (; *format != '\0'; format++)
-	{
-		if (*format != '%')
-		{
-			_putchar(*format);
-=======
 
 /**
   *_printf - Entry for printf
@@ -44,28 +21,15 @@ int _printf(const char *fmt, ...)
 		if (*fmt != '%')
 		{
 			_putchar(*fmt);
->>>>>>> 3a470f541044878857aeb4e55f27581635875bb9
 			i++;
 		}
 		else
 		{
-<<<<<<< HEAD
-			format++;
-			i += valid_args(&format, valist);
-		}
-	}
-
-	va_end(valist);
-
-	return (i);
-}
-=======
 			fmt++;
 			i += valid_args(&fmt, ap);
 		}
 	}
+
 	va_end(ap);
 	return (i);
 }
-
->>>>>>> 3a470f541044878857aeb4e55f27581635875bb9
